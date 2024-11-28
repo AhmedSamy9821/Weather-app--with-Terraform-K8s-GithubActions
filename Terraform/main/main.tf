05c1f334d241831e2dadcad3dc255c38bdaa978f
AhmedSamy9821 <134964599+AhmedSamy9821@users.noreply.github.com>
2024-11-28 03:49:53 +0200

    build infra to test auth service

--- Terraform/main/main.tf
+++ Terraform/main/main.tf
@@ -1,17 +1,17 @@
 ##############
 #vpc module creates vpc and 4 public subnets for eks cluster
 ############## 
-/* module "vpc" {
+ module "vpc" {
   source = "../modules/vpc"
   env = var.env
   vpc_cider = var.vpc_cider
   subnet_cidrs = var.subnet_cidrs
 }
- */
+
 ##############
 #eks_cluster module creates eks cluster and node group
 ##############
-/* module "eks_cluster" {
+module "eks_cluster" {
   source = "../modules/eks"
   env = var.env
   subnet_ids = module.vpc.subnet_ids
@@ -19,14 +19,14 @@
   auto_scaling_config = var.auto_scaling_config
   
 }
- */
+ 
 
 ###############
 #certificate module creates certificate for the domain
 ###############
-/* module "certificate" {
+module "certificate" {
   source = "../modules/domain_Certificate"
   env = var.env
   sub_domain = var.sub_domain
   domain = var.domain
-} */
+} 
