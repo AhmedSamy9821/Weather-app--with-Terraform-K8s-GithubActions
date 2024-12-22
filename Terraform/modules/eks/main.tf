32d0c344cb355bbef601c4d92d2d5186d47f2358
AhmedSamy9821 <ahmedsami2302@gmail.com>
2024-12-22 07:55:05 +0200

    terraform ,eks cluster , attach ebs policy to OIDC provider to be able to manage ebs for eks

--- Terraform/modules/eks/main.tf
+++ Terraform/modules/eks/main.tf
@@ -128,3 +128,39 @@ resource "aws_eks_node_group" "weather-nodegroup" {
   }
 }
 
+
+#Attach EBS IAM role to OIDC to be able to manage EBS
+
+
+locals {
+  OIDC = replace(aws_eks_cluster.weather-cluster.identity.oidc.issuer, "https://", "")
+}
+
+resource "aws_iam_role" "EBS-role" {
+  name = "AmazonEKS_EBS_CSI_DriverRole"
+
+  assume_role_policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Principal": {
+            "Federated": "arn:aws:iam::637423498743:${local.OIDC}"
+          },
+          "Action": "sts:AssumeRoleWithWebIdentity",
+          "Condition": {
+            "StringEquals": {
+              "${local.OIDC}:aud": "sts.amazonaws.com",
+              "${local.OIDC}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+            }
+          }
+        }
+      ]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "EBS-role-attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.EBS-role.name
+}
+
